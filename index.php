fe2a70512379b1e481820924c755328ca1cf6367
Deep Toaster <deeptoaster@gmail.com>
2017-01-10 03:40:29 -0800

    added ballot display

--- index.php
+++ index.php
@@ -19,24 +19,80 @@ echo <<<EOF
 			<h2>$subtitle</h2>
 
 EOF;
-?>			<form>
+?>			<form action="./" method="post">
+<?
+$result = $pdo->prepare(<<<EOF
+SELECT `id`,
+	`name`,
+	`writeins`
+FROM `elections`
+WHERE `closed` IS NULL
+EOF
+	);
+
+$result->execute();
+
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+	$title = htmlentities($row['name'], NULL, 'UTF-8');
+
+	$subresult = $pdo->prepare(<<<EOF
+SELECT `id`,
+	`name`
+FROM `candidates`
+WHERE `election` = :election
+EOF
+		);
+
+	$subresult->execute(array(
+		':election' => $row['id']
+	));
+
+	$subrows = $subresult->fetchAll(PDO::FETCH_ASSOC);
+	$max = count($subrows) + 1;
+
+	echo <<<EOF
 				<fieldset class="poll">
-					<legend>Should Blacker president be decided by a congress of pigeons? Should Blacker president be decided by a congress of pigeons?</legend>
+					<legend>$title</legend>
+
+EOF;
+
+	foreach ($subrows as $subrow) {
+		$name = htmlentities($subrow['name'], NULL, 'UTF-8');
+
+		echo <<<EOF
 					<div class="form-control">
 						<div class="input-group">
-							<input id="option" type="number" />
+							<input id="rank$subrow[id]" name="candidate$subrow[id]" type="number" min="1" max="$max" />
 						</div>
-						<label for="option">Option 1</label>
+						<label for="rank$subrow[id]">$name</label>
 					</div>
+
+EOF;
+	}
+
+	if ($row['writeins']) {
+		echo <<<EOF
 					<div class="form-control">
 						<div class="input-group">
-							<input id="writein" type="number" />
+							<input name="writein-rank$row[id]" type="number" min="1" max="$max" />
 						</div>
 						<div class="input-group input-group-right">
-							<input name="writein" />
+							<input name="writein$row[id]" type="text" placeholder="Write-in" />
 						</div>
 					</div>
+EOF;
+	}
+
+	echo <<<EOF
 				</fieldset>
+
+EOF;
+}
+?>				<div class="form-control">
+					<div class="input-group">
+						<input type="submit" value="Cast Ballots" />
+					</div>
+				</div>
 			</form>
 		</div>
 <?
